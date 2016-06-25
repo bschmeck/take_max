ac5aeaf61b1dc58eec51d94903dca7852c7ee367
Ben Schmeckpeper <bschmeckpeper@groupon.com>
2016-06-24 21:46:45 -0500

    Taking more than are in the list returns the whole list

--- lib/take_max.ex
+++ lib/take_max.ex
@@ -1,4 +1,13 @@
 defmodule TakeMax do
   def take_max(_list, 0, _func), do: []
   def take_max([], _num, _func), do: []
+
+  def take_max(list, num, func) do
+    take_max(list, num, [], func)
+  end
+
+  defp take_max([], _remaining, keepers, _func), do: keepers
+  defp take_max([head | tail], remaining, keepers, func) do
+    take_max(tail, remaining - 1, [head | keepers], func)
+  end
 end
--- test/take_max_test.exs
+++ test/take_max_test.exs
@@ -9,4 +9,10 @@ defmodule TakeMaxTest do
   test "taking from an empty list returns an empty list" do
     assert TakeMax.take_max([], 5, &(&1)) == []
   end
+
+  test "taking more elements than are in the list returns everything in the list" do
+    source = [1, 2, 3]
+    taken = TakeMax.take_max(source, 20, &(&1))
+    assert Enum.sort(source) == Enum.sort(taken)
+  end
 end
