fd44ae08f452c71bc95d24cec1370aabec25cf04
Ben Schmeckpeper <bschmeckpeper@groupon.com>
2016-06-24 21:53:37 -0500

    Test that the sorting function is used

--- test/take_max_test.exs
+++ test/take_max_test.exs
@@ -22,4 +22,10 @@ defmodule TakeMaxTest do
     taken = TakeMax.take_max(source, 3, &(&1))
     assert Enum.sort(taken) == [700, 800, 900]
   end
+
+  test "it sorts elements in the list by the given function" do
+    source = ["long string", "abc", "this is really long", "foobar", "nope, this is the longest"]
+    taken = TakeMax.take_max(source, 2, &String.length/1)
+    assert Enum.sort(taken) == ["nope, this is the longest", "this is really long"]
+  end
 end
