2d0010d1aa8f2d9137b3c5e30e45780f3b6c8e38
Ben Schmeckpeper <bschmeckpeper@groupon.com>
2016-06-24 21:45:40 -0500

    Base case tests and implementation

--- lib/take_max.ex
+++ lib/take_max.ex
@@ -1,2 +1,4 @@
 defmodule TakeMax do
+  def take_max(_list, 0, _func), do: []
+  def take_max([], _num, _func), do: []
 end
--- test/take_max_test.exs
+++ test/take_max_test.exs
@@ -2,7 +2,11 @@ defmodule TakeMaxTest do
   use ExUnit.Case
   doctest TakeMax
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "taking 0 elements returns an empty list" do
+    assert TakeMax.take_max([1,2,3], 0, &(&1)) == []
+  end
+
+  test "taking from an empty list returns an empty list" do
+    assert TakeMax.take_max([], 5, &(&1)) == []
   end
 end
